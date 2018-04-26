7246e82bc2ceec3828f55557c6467fa4ccb7b8c3
NULL <null@nil.com>
2018-04-26 19:59:23 +0800

    update cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "NET: ${prefix}0/24 --ifconfig ${prefix}1 ${prefix}2"
 
 iptables -t nat -I POSTROUTING -s "${prefix}0/24" -j MASQUERADE
 
-openvpn --dev tun --proto tcp-server --mssfix 1472 --comp-lzo no --fast-io --ifconfig ${prefix}1 ${prefix}2 2>&1 > /var/ovpn-tcp.log &
+openvpn --dev tun --proto tcp-server --comp-lzo no --fast-io --ifconfig ${prefix}1 ${prefix}2 2>&1 > /var/ovpn-tcp.log &
 
-ss-server -s 0.0.0.0 -p $SERVER_PORT -k ${PASSWORD:-$(hostname)} -m $METHOD -t $TIMEOUT --fast-open --no-delay ${ARGS} 2>&1 > /var/ss-tcp.log
+ss-server -s 0.0.0.0 -p $SERVER_PORT -k ${PASSWORD:-$(hostname)} -m $METHOD -t $TIMEOUT --fast-open --no-delay ${ARGS} 2>&1 > /var/ovpn-ss.log
+
