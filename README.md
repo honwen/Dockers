fc665c1e21d1339a70c2cc73846667b8a3e9ebc4
honwen.chan <honwen.chan@gmail.com>
2020-12-12 16:08:26 +0800

    xray: vless with xtls, minimal

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-### Docker
-- https://hub.docker.com/r/chenhw2
+### Source
+
+- https://github.com/chenhw2/Dockers/tree/xray
+
+### Thanks
+
+- https://github.com/XTLS/Xray-core
+
+### Usage
+
+```
+$ docker pull chenhw2/xray
+
+$ docker run -d \
+    --name xray \
+    -e ACME_DOMAIN=xray.example.org \
+    -e USERS=$(uuidgen) \
+    -v /etc/ssl/caddy:/etc/ssl/caddy \
+    -p 443:443 \
+    chenhw2/xray:latest
+```
---
+++ entrypoint.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+XRAY_CONFIG=/etc/xray.d/server.json
+XRAY_SSLPORT=443
+XRAY_FALLBACK=80
+ACME_PREFIX=${ACME_PREFIX:-/etc/ssl/caddy/acme/acme-v02.api.letsencrypt.org/sites}
+ACME_DOMAIN=${ACME_DOMAIN:-example.org}
+
+# Xray config Init
+cat <<EOF | sed '/DELETE/d' | jq '.' | tee $XRAY_CONFIG
+{
+  "log": {
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "port": ${XRAY_SSLPORT},
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+$(echo $USERS | sed 's_;_\n_g' | while read user; do
+uuid=$(echo -n $user | sed 's_:.*__g')
+email=$(echo -n $user | sed 's_.*:__g')
+echo '{}' | jq ".|{uuid:\"$uuid\",email:\"$email\",flow:\"xtls-rprx-direct\"}"
+echo ','
+done
+)====TO-DELETE====
+        ],
+        "decryption": "none",
+        "fallbacks": [
+          {
+            "dest": ${XRAY_FALLBACK}
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "security": "xtls",
+        "xtlsSettings": {
+          "alpn": ["http/1.1"],
+          "certificates": [
+            {
+              "certificateFile": "${ACME_PREFIX}/${ACME_DOMAIN}/${ACME_DOMAIN}.crt",
+              "keyFile": "${ACME_PREFIX}/${ACME_DOMAIN}/${ACME_DOMAIN}.key"
+            }
+          ]
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+/usr/bin/xray run -c $XRAY_CONFIG
